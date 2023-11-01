abcf2c78cda5388300c3cb437df0388ae7df0435
Alex <alex.akten@outlook.com>
2023-11-01 12:19:05 +1100

    navbar fixes

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import forms from '@tailwindcss/forms';
 
 const config: Config = {
   content: [
@@ -27,6 +28,8 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    forms,
+  ],
 };
 export default config;
