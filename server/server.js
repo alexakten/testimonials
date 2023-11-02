1a9e9a5e53db852096467121698132ec4cf91edb
Alex <alex.akten@outlook.com>
2023-11-02 14:42:39 +1100

    wrapped Router in wrapper to render on client

--- server/server.js
+++ server/server.js
@@ -7,6 +7,11 @@ const express = require("express");
 const app = express();
 const port = 3003; // You can choose another port if you want
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 app.use(cors()); // Enabling CORS for all routes
 app.use(express.json()); // Parse incoming request bodies as JSON
 
